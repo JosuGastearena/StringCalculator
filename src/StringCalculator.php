e305695bff0f8ad8547146c8b332c978247ece32
Josu Gastearena <jgastearenasalgado@gmail.com>
2022-02-28 07:45:43 +0000

    First Step - Test given_an_empty_string_return_0 passed

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -1,6 +1,12 @@
 <?php
 
+namespace Deg540\PHPTestingBoilerplate;
+
 class StringCalculator
 {
+    public function add(String $number): String
+    {
+        return 0;
+    }
 
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -8,5 +8,15 @@ use Deg540\PHPTestingBoilerplate\StringCalculator;
 
 class StringCalculatorTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function given_an_empty_string_return_0(){
+
+        $stringCalculator = new StringCalculator();
+        $calculatedString = $stringCalculator->add("");
+        $this->assertEquals(0, $calculatedString);
+
+    }
 
 }
