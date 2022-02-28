fb6ab47907063a77eecb73fbb3e6e1640bfcdf58
Josu Gastearena <jgastearenasalgado@gmail.com>
2022-02-28 16:56:24 +0000

    Multiple errors - Test given_an_unknown_number_of_arguments_with_multiple_errors_return_multiple_error_message passed

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -7,81 +7,127 @@ class StringCalculator
 {
     public function add(String $number): String
     {
-        if(empty($number)){
+        if(empty($number))
+        {
             return 0;
         }
 
+        $correctNumber = true;
+        $errorString = "";
         $delimiters = ",\n";
         $positionOfCustomDelimiterAndNumbersSeparator = -1;
-        if($number[0] == "/" && $number[1] == "/"){
+
+        if($number[0] == "/" && $number[1] == "/")
+        {
             $positionOfCustomDelimiterAndNumbersSeparator = strpos($number, "\n");
             $delimiters = substr($number, 2, $positionOfCustomDelimiterAndNumbersSeparator - 2);
         }
+
         $numbers = substr($number, $positionOfCustomDelimiterAndNumbersSeparator + 1);
 
         $badUseOfSeparatorsChecked = $this->checkOutBadUsedSeparators($numbers);
-        if($badUseOfSeparatorsChecked[0]){
-            return "Number expected but " . $badUseOfSeparatorsChecked[1] . " found at position " . $badUseOfSeparatorsChecked[2];
+        if($badUseOfSeparatorsChecked[0])
+        {
+            $correctNumber = false;
+            if(!empty($errorString))
+            {
+                $errorString .= "\n";
+            }
+            $errorString .= "Number expected but " . $badUseOfSeparatorsChecked[1] . " found at position " . $badUseOfSeparatorsChecked[2];
         }
 
         $separatorInLastPosition = $this->checkOutSeparatorInLastPosition($numbers);
-
-        if($separatorInLastPosition){
-            return "Number expected but EOF found";
+        if($separatorInLastPosition)
+        {
+            $correctNumber = false;
+            if(!empty($errorString))
+            {
+                $errorString .= "\n";
+            }
+            $errorString .= "Number expected but EOF found";
         }
-
 
         $separatedNumber = preg_split('/[' . $delimiters . ']/', $numbers, -1, PREG_SPLIT_NO_EMPTY);
         $sum = 0;
-        for($i = 0; $i < count($separatedNumber); $i++){
-            if(!is_numeric($separatedNumber[$i])){
+        $firstNegativeFound = false;
+        for($i = 0; $i < count($separatedNumber); $i++)
+        {
+            if(!is_numeric($separatedNumber[$i]))
+            {
                 $incorrectSeparator = $this->findOutIncorrectSeparator($separatedNumber[$i]);
-                return "'$delimiters' expected but '$incorrectSeparator' found at position " . strpos($numbers, $incorrectSeparator) . ".";
+                $correctNumber = false;
+                if(!empty($errorString))
+                {
+                    $errorString .= "\n";
+                }
+                $errorString .= "'$delimiters' expected but '$incorrectSeparator' found at position " . strpos($numbers, $incorrectSeparator) . ".";
             }
-            else if($separatedNumber[$i] < 0){
+            else if($separatedNumber[$i] < 0 && !$firstNegativeFound)
+            {
                 $negativeList = $this->manageNegativeNumbers($separatedNumber, $i);
-                return "Negative not allowed: $negativeList";
+                $correctNumber = false;
+                $firstNegativeFound = true;
+                if(!empty($errorString))
+                {
+                    $errorString .= "\n";
+                }
+                $errorString .= "Negative not allowed: $negativeList";
             }
-            $sum += $separatedNumber[$i];
+            else
+            {
+                $sum += $separatedNumber[$i];
+            }
+        }
+        if(!$correctNumber)
+        {
+            return $errorString;
         }
         return $sum;
-
     }
 
     public function checkOutBadUsedSeparators($number): array
     {
-        if(str_contains($number, ",,")){
+        if(str_contains($number, ",,"))
+        {
             return [true, ",", (strpos($number, ",,") + 1)];
         }
-        else if(str_contains($number, ",\n")){
+        else if(str_contains($number, ",\n"))
+        {
             return [true, "\n", (strpos($number, ",\n") + 1)];
         }
-        else if(str_contains($number, "\n\n")){
+        else if(str_contains($number, "\n\n"))
+        {
             return [true, "\n", (strpos($number, ",\n") + 1)];
         }
-        else if(str_contains($number, "\n,")){
+        else if(str_contains($number, "\n,"))
+        {
             return [true, ",", (strpos($number, "\n,") + 1)];
         }
         return [false, "", ""];
     }
 
-    public function checkOutSeparatorInLastPosition($number): bool{
-        if($number[strlen($number) - 1] == "," || $number[strlen($number) - 1] == "\n"){
+    public function checkOutSeparatorInLastPosition($number): bool
+    {
+        if($number[strlen($number) - 1] == "," || $number[strlen($number) - 1] == "\n")
+        {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
 
-    public function findOutIncorrectSeparator($separatedNumbersElement): String{
+    public function findOutIncorrectSeparator($separatedNumbersElement): String
+    {
 
         $permitedValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
         $incorrectSeparator = "";
-        for($i = 0; $i < strlen($separatedNumbersElement); $i++){
-            if(!in_array($separatedNumbersElement[$i], $permitedValues)){
+        for($i = 0; $i < strlen($separatedNumbersElement); $i++)
+        {
+            if(!in_array($separatedNumbersElement[$i], $permitedValues))
+            {
                 $incorrectSeparator = $separatedNumbersElement[$i];
-
             }
         }
         return $incorrectSeparator;
@@ -89,18 +135,20 @@ class StringCalculator
 
     }
 
-    public function manageNegativeNumbers($separatedNumber, $index): String{
+    public function manageNegativeNumbers($separatedNumber, $index): String
+    {
         $negativeList = "";
-        for($i = $index; $i < count($separatedNumber); $i++){
-            if($separatedNumber[$i] < 0){
-                if(!empty($negativeList)){
+        for($i = $index; $i < count($separatedNumber); $i++)
+        {
+            if($separatedNumber[$i] < 0)
+            {
+                if(!empty($negativeList))
+                {
                     $negativeList .= ", ";
                 }
                 $negativeList .= $separatedNumber[$i];
             }
-            }
+        }
         return $negativeList;
     }
-
-
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 namespace Deg540\PHPTestingBoilerplate\Test;
 use PHPUnit\Framework\TestCase;
 use Deg540\PHPTestingBoilerplate\StringCalculator;
@@ -13,11 +11,9 @@ class StringCalculatorTest extends TestCase
      */
     public function given_an_empty_string_return_0()
     {
-
         $stringCalculator = new StringCalculator();
         $calculatedString = $stringCalculator->add("");
         $this->assertEquals(0, $calculatedString);
-
     }
 
     /**
@@ -25,11 +21,9 @@ class StringCalculatorTest extends TestCase
      */
     public function given_an_unknown_number_of_arguments_return_their_sum()
     {
-
         $stringCalculator = new StringCalculator();
         $calculatedString = $stringCalculator->add("1.1,2.2,3.3");
         $this->assertEquals(6.6, $calculatedString);
-
     }
 
     /**
@@ -37,11 +31,9 @@ class StringCalculatorTest extends TestCase
      */
     public function given_an_unknown_number_of_arguments_with_coma_and_newline_separators_return_their_sum()
     {
-
         $stringCalculator = new StringCalculator();
         $calculatedString = $stringCalculator->add("1.1,2.2\n3.3");
         $this->assertEquals(6.6, $calculatedString);
-
     }
 
     /**
@@ -49,11 +41,9 @@ class StringCalculatorTest extends TestCase
      */
     public function given_an_unknown_number_of_arguments_with_coma_and_nextline_separators_bad_used_return_error_message()
     {
-
         $stringCalculator = new StringCalculator();
         $calculatedString = $stringCalculator->add("1.1,2.2,\n3.3");
         $this->assertEquals("Number expected but \n found at position 8", $calculatedString);
-
     }
 
     /**
@@ -61,11 +51,9 @@ class StringCalculatorTest extends TestCase
      */
     public function given_an_unknown_number_of_arguments_with_separator_in_last_position_return_error_message()
     {
-
         $stringCalculator = new StringCalculator();
         $calculatedString = $stringCalculator->add("1,3,");
         $this->assertEquals("Number expected but EOF found", $calculatedString);
-
     }
 
     /**
@@ -73,11 +61,9 @@ class StringCalculatorTest extends TestCase
      */
     public function given_an_unknown_number_of_arguments_with_custom_separator_return_their_sum()
     {
-
         $stringCalculator = new StringCalculator();
         $calculatedString = $stringCalculator->add("//;\n1;2");
         $this->assertEquals("3", $calculatedString);
-
     }
 
     /**
@@ -85,11 +71,9 @@ class StringCalculatorTest extends TestCase
      */
     public function given_an_unknown_number_of_arguments_with_custom_separator_sep_return_their_sum()
     {
-
         $stringCalculator = new StringCalculator();
         $calculatedString = $stringCalculator->add("//sep\n1sep2sep1.5");
         $this->assertEquals("4.5", $calculatedString);
-
     }
 
     /**
@@ -97,11 +81,9 @@ class StringCalculatorTest extends TestCase
      */
     public function given_an_unknown_number_of_arguments_with_custom_separator_bad_used_return_error_message()
     {
-
         $stringCalculator = new StringCalculator();
         $calculatedString = $stringCalculator->add("//sep\n1sep2,1.5");
         $this->assertEquals("'sep' expected but ',' found at position 5.", $calculatedString);
-
     }
 
     /**
@@ -114,6 +96,13 @@ class StringCalculatorTest extends TestCase
         $this->assertEquals("Negative not allowed: -2, -3", $calculatedString);
     }
 
-
-
+    /**
+     * @test
+     */
+    public function given_an_unknown_number_of_arguments_with_multiple_errors_return_multiple_error_message()
+    {
+        $stringCalculator = new StringCalculator();
+        $calculatedString = $stringCalculator->add("1,,-2");
+        $this->assertEquals("Number expected but , found at position 2\nNegative not allowed: -2", $calculatedString);
+    }
 }
