a42263987015dde4196350077f4cb3cd24707483
Josu Gastearena <jgastearenasalgado@gmail.com>
2022-02-28 10:10:11 +0000

    NewLine as Separator - given_an_unknown_number_of_arguments_with_coma_and_nextline_separators_bad_used_return_error_message

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -2,6 +2,7 @@
 
 namespace Deg540\PHPTestingBoilerplate;
 
+
 class StringCalculator
 {
     public function add(String $number): String
@@ -9,6 +10,12 @@ class StringCalculator
         if(empty($number)){
             return 0;
         }
+
+        $badUseOfDelimitersChecked = $this->checkOutBadUsedDelimiters($number);
+        if($badUseOfDelimitersChecked[0]){
+            return "Number expected but " . $badUseOfDelimitersChecked[1] . " found at position " . $badUseOfDelimitersChecked[2];
+        }
+
         $numberSeparatorsPrepared = str_replace("\n", ",", $number);
         $comaSeparatedNumber = explode(",", $numberSeparatorsPrepared);
         $sum = 0;
@@ -17,6 +24,29 @@ class StringCalculator
         }
         return $sum;
 
+    }
+
+
+
+
+
+
+
+
+    public function checkOutBadUsedDelimiters($number): array{
+        if(str_contains($number, ",,")){
+            return [true, ",", (strpos($number, ",,") + 1)];
+        }
+        else if(str_contains($number, ",\n")){
+            return [true, "\n", (strpos($number, ",\n") + 1)];
+        }
+        else if(str_contains($number, "\n\n")){
+            return [true, "\n", (strpos($number, ",\n") + 1)];
+        }
+        else if(str_contains($number, "\n,")){
+            return [true, ",", (strpos($number, "\n,") + 1)];
+        }
+        return [false, "", ""];
     }
 
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -41,6 +41,16 @@ class StringCalculatorTest extends TestCase
 
     }
 
+    /**
+     * @test
+     */
+    public function given_an_unknown_number_of_arguments_with_coma_and_nextline_separators_bad_used_return_error_message(){
+
+        $stringCalculator = new StringCalculator();
+        $calculatedString = $stringCalculator->add("1.1,2.2,\n3.3");
+        $this->assertEquals("Number expected but \n found at position 8", $calculatedString);
+
+    }
 
 
 
