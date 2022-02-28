da15b61d6b7354d3d18f8de367dccaefc1852fd0
Josu Gastearena <jgastearenasalgado@gmail.com>
2022-02-28 07:30:39 +0000

    Base prepared for StringCalculator kata

---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+namespace Deg540\PHPTestingBoilerplate\Test;
+use PHPUnit\Framework\TestCase;
+use Deg540\PHPTestingBoilerplate\StringCalculator;
+
+class StringCalculatorTest extends TestCase
+{
+
+}
